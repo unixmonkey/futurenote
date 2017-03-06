54a9cb72f1fe957411dc1e1864af4f66ce800e3a
David Jones <unixmonkey1@gmail.com>
2017-03-06 18:34:27 -0500

    Add some basic components and route matching.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Router, Route, Redirect, browserHistory } from 'react-router'
 import Header from './Header'
+import Login from './Login'
+import Notes from './Notes'
 
 class App extends Component {
   render() {
     return (
       <div className="container wrap">
         <Header/>
-
+        <Router history={browserHistory}>
+          <Redirect from="/" to="/notes" />
+          <Route path="/login" component={Login}/>
+          <Route path="/notes" component={Notes}/>
+        </Router>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
