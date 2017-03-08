5df882ff71210801624d2453d078e45ede7359df
David Jones <unixmonkey1@gmail.com>
2017-03-08 16:54:35 -0500

    Add basic notes loop.

--- src/Notes.js
+++ src/Notes.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 
 class Notes extends Component {
+  constructor() {
+    super();
+    this.state = {
+      notes: [
+        { id: 1, title: 'First Note', body: 'This is the first note' },
+        { id: 2, title: 'Second Note', body: 'This is the second note' },
+        { id: 3, title: 'Third Note', body: 'This is the third note' }
+      ]
+    };
+  }
+
   render() {
     return (
-      <h1>Notes</h1>
+      <div>
+        <h1>Notes</h1>
+        <ul>
+          {this.state.notes.map((note, i) =>
+            <li key={i}>{note.title}</li>
+          )}
+        </ul>
+      </div>
     );
   }
 }
